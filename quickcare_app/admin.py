4acf9b84a0c757b5423ffd14af5b95e765c0c321
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-03-20 04:53:27 +0500

    admin.py updated

--- quickcare_app/admin.py
+++ quickcare_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Doctor, Patient, Notification, Queue, Emergency, Room, Department
+from user.models import User
 
-# Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Notification)
+admin.site.register(Queue)
+admin.site.register(Emergency)
+admin.site.register(Room)
+admin.site.register(Department)
