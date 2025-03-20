f7889f230425ad34c40f1e0fe5da598d3d4edf28
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-03-20 21:05:33 +0500

    added serializer

---
+++ quickcare_app/serializers/doctor_patient.py
@@ -0,0 +1,69 @@
+from rest_framework import serializers
+from quickcare_app.models import Doctor, Patient
+from django.utils.timezone import now
+import datetime
+import re
+from datetime import date
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    department_name = serializers.CharField(source="department.name", read_only=True)
+    room_number = serializers.CharField(source="room.room_number", read_only=True)
+
+    class Meta:
+        model = Doctor
+        fields = ['id', 'full_name', 'specialization', 'phone', 'department_name', 'room_number']
+
+    def validate_phone(self, value):
+        pattern = re.compile(r"^\+?998\d{9}$")
+        if not pattern.match(value):
+            raise serializers.ValidationError("❌ Yaroqsiz telefon raqam! Format: +998901234567")
+        return value
+
+class PatientSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    age = serializers.SerializerMethodField()
+    blood_type_display = serializers.CharField(source='get_blood_type_display' , read_only=True)
+
+    class Meta:
+        model = Patient
+        fields = ['id', 'full_name', 'age', 'user', 'blood_type_display',
+                  'phone_number', 'birth_date', 'address', 'emergency_contact', 'medical_history', 'allergies', 'chronic_diseases', 'complaints', 'created_at']
+        read_only_fields = ['id', 'full_name', "age", "created_at"]
+
+    def get_full_name(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}".strip()
+
+    def get_age(self, obj):
+        if obj.birth_date:
+            today = date.today()
+            return (
+                    today.year - obj.birth_date.year
+                    - ((today.month, today.day) < (obj.birth_date.month, obj.birth_date.day))
+            )
+        return None
+
+    def validate_phone(self, value):
+        pattern = re.compile(r"^\+?998\d{9}$")
+        if not pattern.match(value):
+            raise serializers.ValidationError("❌ Yaroqsiz telefon raqam! Format: +998901234567")
+        return value
+
+    def create(self, validated_data):
+        user = validated_data.pop['user']
+        patient = Patient.objects.create(user=user, **validated_data)
+        return patient
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user", None)
+        if user_data:
+            user = instance.user
+            for attr, value in user_data.items():
+                setattr(user, attr, value)
+            user.save()
+
+        return super().update(instance, validated_data)
+
+
+
