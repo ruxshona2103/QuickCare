f9b64af30c176b945e597420198f826f417dc45b
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-04-01 16:51:30 +0500

    updated

--- quickcare_app/urls.py
+++ quickcare_app/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from quickcare_app.views import DoctorViewSet, PatientViewSet, EmergencyViewSet, AmbulanceViewSet
+from quickcare_app.views import (DoctorViewSet, PatientViewSet,
+                                 EmergencyViewSet, AmbulanceViewSet,
+                                 MedicineViewSet, PharmacyViewSet, PatiantMedicineViewSet)
 
 router = DefaultRouter()
 router.register(r'doctors', DoctorViewSet)
 router.register(r'patients', PatientViewSet)
 router.register(r'emergency', EmergencyViewSet)
 router.register(r'ambulance', AmbulanceViewSet)
+router.register(r'medicine', MedicineViewSet)
+router.register(r'pharmacy', PharmacyViewSet)
+router.register(r'patientmedicine', PatiantMedicineViewSet)
 
 
 urlpatterns = [
